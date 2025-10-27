06918c387e88059415d0b038ff58bd68b6c359e3
unknown <farahk291206@gmail.com>
2025-10-28 00:03:17 +0300

    init

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Route,Routes } from 'react-router-dom'
+
+
+createRoot(document.getElementById('root')).render(
+ 
+   <BrowserRouter>  
+   <App />
+   </BrowserRouter>
+ 
+)
