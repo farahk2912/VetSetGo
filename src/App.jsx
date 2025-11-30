569150e556c2e3381da01ba5dbdb5261dac2a149
Malak4242 <152904703+Malak4242@users.noreply.github.com>
2025-11-30 19:47:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Navbar from "./Components/Navbar";
-import Profile from "./pages/Profile"; // Changed from profile to Profile
+import Profile from "./pages/profile"; // Changed from profile to Profile
 import { PetProvider } from "./Components/petContext"; 
 
 function App() {
