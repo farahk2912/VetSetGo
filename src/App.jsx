df0e2bfcc5a5a2e3846ff1c83801360a22be0f30
malak4242 <malakhabak56@gmail.com>
2025-11-30 19:56:28 +0200

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./pages/Home";
 import Navbar from "./Components/Navbar";
-
-
+import Profile from "./pages/profile"; // Changed from profile to Profile
+import { PetProvider } from "./Components/petContext"; 
 
 function App() {
   return (
