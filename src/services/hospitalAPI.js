82684625b9c9be9c979b93a58c3b311665732329
malak4242 <malakhabak56@gmail.com>
2025-12-13 02:16:45 +0200

    tr4

--- src/services/hospitalAPI.js
+++ src/services/hospitalAPI.js
@@ -1,5 +1,5 @@
 // src/services/hospitalAPI.js
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';;
+const API_URL = "https://vetsetgoback-production-14d6.up.railway.app" || 'http://localhost:5000';;
 // const API_URL =  'http://localhost:5000';
 
 export const hospitalAPI = {
